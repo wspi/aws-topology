8e0d7196d074901aa4d2df2795977ee858745c8f
Richard Worwood <richard@worwood.com>
2019-11-20 12:39:35 +0000

    Initial Setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aws-topology',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='richardw',
+    author_email='',
+    description=''
+)
